a195af14d9247a40e99905953f141f16c379d288
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-04-06 10:09:40 -0400

    Fif0

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, "s" if argc != 1 else ""))
+
+        for i in range(argc):
+            print("{}: {}".format(i + 1, argv[i + 1]))
