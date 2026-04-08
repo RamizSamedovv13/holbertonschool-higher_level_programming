db5abc00a5e95f9676d403ba21ab0c748c68a8f3
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-04-08 11:00:51 -0400

    Fix1

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
+    if matrix == [[]]:
+        print()
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
