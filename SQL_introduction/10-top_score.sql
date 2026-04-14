4f95d20ee651f7c387e03d2623744c42f5cdf8af
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-04-14 12:43:59 -0400

    Fix1

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,8 @@
+-- List all records in second_table ordered by score descending
+SELECT
+    score,
+    name
+FROM
+    `second_table`
+ORDER BY
+    score DESC
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,10 @@
+-- Count records grouped by score, ordered by count descending
+SELECT
+    score,
+    COUNT(*) AS number
+FROM
+    `second_table`
+GROUP BY
+    score
+ORDER BY
+    number DESC
