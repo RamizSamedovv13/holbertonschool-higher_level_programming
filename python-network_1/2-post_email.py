02035a281b1368dde9e48c9bea4ebb9fe45e6d16
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-04-17 06:51:04 -0400

    Fix2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Send POST request with email"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({"email": email}).encode("utf-8")
+
+    req = urllib.request.Request(url, data=data)
+
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode("utf-8")
+        print(body)
