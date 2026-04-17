1bea0bc40dd4e1dc7cb1cd41ff7f2fb76b418eb2
RamizSəmədovv13 <ramizsmdov11@gmail.com>
2026-04-17 06:40:50 -0400

    Fix2

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url) as response:
         body = response.read()
+        text = body.decode("utf-8")
 
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
+        print("\t- utf8 content:", text)
